278c507b9c18cb7caed55d94ccdc79bba0e0bf7c
Chris Campbell <ccampbell@climateinteractive.org>
2022-08-10 19:12:01 -0700

    fix: remove pluggable GA script

--- packages/docs-builder/src/gen-html.ts
+++ packages/docs-builder/src/gen-html.ts
@@ -183,20 +183,6 @@ export function writeHtmlFile(
   htmlPage: HtmlPage,
   templateName: string
 ): void {
-  // Prepare the Google Analytics script
-  // TODO: Only if gaTrackingId is defined
-  // const analyticsScript = `<script>
-  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  // ga('create', '${gaTrackingId}', 'auto');
-  // if (window.location.hostname === '${gaEnabledHost}') {
-  //   ga('send', 'pageview')
-  // }
-  // </script>`
-  const analyticsScript = ''
-
   // TODO: Compute the correct relative path without assuming structure of En-ROADS User Guide
   // (which uses a `guide` subdirectory)
   const basePath = htmlPage.relPath.startsWith('guide') ? '..' : '.'
@@ -326,8 +312,6 @@ export function writeHtmlFile(
         return `${basePath}/${assets.get('base.css')}`
       case 'PROJECT_CSS_PATH':
         return `${basePath}/${assets.get('project.css')}`
-      case 'ANALYTICS_SCRIPT':
-        return analyticsScript
       case 'LUNR_PATH':
         return `${basePath}/${assets.get('lunr.min.js')}`
       // case 'LUNR_STEMMER_SUPPORT':
