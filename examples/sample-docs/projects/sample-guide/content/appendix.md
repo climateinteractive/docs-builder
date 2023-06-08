1e6ae0edcb55d10239ca6179e04f8782ad208dec
Chris Campbell <ccampbell@climateinteractive.org>
2023-06-08 15:36:26 -0700

    fix: include CF4 in list of automatically subscripted chemicals (#37)
    Fixes #36

--- examples/sample-docs/projects/sample-guide/content/appendix.md
+++ examples/sample-docs/projects/sample-guide/content/appendix.md
@@ -3,3 +3,11 @@
 This is the appendix.  The text is pink because of a project- and page-specific CSS override.
 
 (This is an example of a page that is only in English and not made available for translation.)
+
+The following chemical formulas should be subscripted automatically:
+* CO2
+* CF4
+* CH4
+* N2O
+* NF3
+* SF6
--- packages/docs-builder/src/gen-html.ts
+++ packages/docs-builder/src/gen-html.ts
@@ -547,6 +547,7 @@ export function writeErrorHtmlFile(context: Context, mdRelPath: string, error: E
 
 const subscriptMap = new Map([
   ['CO2', 'CO<sub>2</sub>'],
+  ['CF4', 'CF<sub>4</sub>'],
   ['CH4', 'CH<sub>4</sub>'],
   ['N2O', 'N<sub>2</sub>O'],
   ['NF3', 'NF<sub>3</sub>'],
@@ -556,8 +557,9 @@ const subscriptMap = new Map([
 /**
  * Replace non-subscript forms of common chemicals with their subscripted equivalent.
  * For example, "CO2" will be converted to "CO<sub>2</sub>".  This only handles
- * the following common forms:
+ * the following common forms (defined in the map above):
  *   CO2
+ *   CF4
  *   CH4
  *   N2O
  *   NF3
@@ -570,7 +572,7 @@ function subscriptify(s: string): string {
   // XXX: Some historical graph images in the En-ROADS User Guide have
   // {CO2,CH4,N2O} in the file name, so this regex is set up to avoid
   // converting those filenames
-  return s.replace(/(Hist_)?(CO2|CH4|N2O|NF3|SF6)/g, (m, m1, m2) => {
+  return s.replace(/(Hist_)?(CO2|CF4|CH4|N2O|NF3|SF6)/g, (m, m1, m2) => {
     if (m1) {
       return m
     } else {
