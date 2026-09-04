80c51e7b35a886c1c80e401cefb6ee2841c81d51
Chris Campbell <ccampbell@climateinteractive.org>
2026-09-03 17:35:34 -0700

    fix: update copyright headers

--- packages/docs-builder/src/build-queue.spec.ts
+++ packages/docs-builder/src/build-queue.spec.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2026 Climate Interactive / New Venture Fund. All rights reserved.
+// Copyright (c) 2026 Climate Interactive / New Venture Fund
 
 import { describe, expect, it } from 'vitest'
 
